f09c5aca5cf6a1feb292750ea5bccb5e1bc23673
AntonShiva <ryashinanthon@yandex.ru>
2025-02-10 12:26:48 +0300

    Initial Commit

---
+++ VPN Shivaya/VPN_ShivayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPN_ShivayaApp.swift
+//  VPN Shivaya
+//
+//  Created by Anton Rasen on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct VPN_ShivayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
